c7655e4eb29809cb391d1640a4003d79bdb123d0
Malik Saqib Iftikhar <malik.saqib@rnssol.com>
2023-04-10 12:49:16 +0500

    simple minter

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,22 @@ pub struct Contract;
 
 #[contractimpl]
 impl Contract {
-    pub fn hello(env: Env, to: Symbol) -> Vec<Symbol> {
-        vec![&env, Symbol::short("Hello"), to]
-    }
+   pub fn ping(env: Env, from: Symbol) -> Vec<Symbol> {
+       vec![&env, Symbol::short("Hello"), from]
+   }
+
+   pub fn mint_nft(env: Env, name: Symbol, token_uri: Symbol) {
+       env.storage().set(&name, &token_uri);
+   }
+
+   pub fn get_nft(env:Env, name: Symbol) -> Symbol {
+       let symbol_result = env.storage().get_unchecked(&name);
+       let symbol: Symbol = match symbol_result {
+           Ok(s) => s,
+           Err(_e) => {
+               Symbol::short("error")
+           }
+       };
+       return symbol
+   }
 }
